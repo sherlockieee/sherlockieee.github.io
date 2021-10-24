deee04e0765a02b4a0722d8645b0abf34be64ff1
sherlockieee <phuonghatrannguyen00@gmail.com>
2021-10-24 14:09:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# "Borderless" Theme, Built with Gatsby
+
+- **[Documents (Korean)](<https://github.com/junhobaik/junhobaik.github.io/wiki/Document-(Borderless)>)**  
+- **[Documents (English)](<https://github.com/junhobaik/junhobaik.github.io/wiki/Document-(Borderless,-en)>)**
+
+---
+
+Gatsby로 제작된 정적 웹사이트,  
+블로그 Borderless 테마입니다.
+
+이 문서에서는 기본적으로 Github Pages로 배포하는 것을 기준으로 작성하였습니다.
+
+**[Borderless DEMO WebSite](https://junhobaik.github.io)**
+
+- 선(Border)이 없는, 색이 최소화된 심플한 디자인
+- 마크다운 게시물 작성 (Markdown + emoji, ktex)
+- 검색 엔진 최적화 (SEO)
+- 사파리 브라우저 등 게시물내 읽기 모드(Reader Mode) 지원
+- Google Analytics 지원
+- Google Adsense 지원
+- Disqus 댓글 기능 지원
+
+## Get Started
+
+자세한 가이드는 [Documents](<https://github.com/junhobaik/junhobaik.github.io/wiki/Document-(Borderless)>)를 확인해주세요.
---
+++ _config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  /** Site MetaData (Required all)*/
+  title: `Hello, World`,                           // (* Required)
+  description: `Thoughts, loosely organized.`,          // (* Required)
+  author: `Ha (sherlockieee)`,                         // (* Required)
+  language: 'en',                        // (* Required) html lang, ex. 'en' | 'en-US' | 'ko' | 'ko-KR' | ...
+  siteUrl: 'https://sherlockieee.github.io',                      // (* Required)
+    // ex.'https://junhobaik.github.io'
+    // ex.'https://junhobaik.github.io/' << X, Do not enter "/" at the end.
+
+  /** Header */
+  profileImageFileName: 'icon.png', // include filename extension ex.'profile.jpg'
+    // The Profile image file is located at path "./images/"
+    // If the file does not exist, it is replaced by a random image.
+
+  /** Home > Bio information*/
+  comment: 'Thoughts, loosely organized.',
+  name: 'Ha Tran Nguyen Phuong',
+  company: 'Minerva University',
+  location: 'San Francisco',
+  email: 'phuonghatrannguyen00@gmail.com',
+  website: '',           // ex.'https://junhobaik.github.io'
+  linkedin: 'https://www.linkedin.com/in/trannguyenphuongha/',                                                          // ex.'https://www.linkedin.com/in/junho-baik-16073a19ab'
+  facebook: '',                                                          // ex.'https://www.facebook.com/zuck' or 'https://www.facebook.com/profile.php?id=000000000000000'
+  instagram: '',                                                         // ex.'https://www.instagram.com/junhobaik'
+  github: 'https://github.com/sherlockieee',                                                            // ex.'https://github.com/junhobaik'
+
+  /** Post */
+  enablePostOfContents: true,     // TableOfContents activation (Type of Value: Boolean. Not String)
+  disqusShortname: '',            // comments (Disqus sort-name)
+  enableSocialShare: true,        // Social share icon activation (Type of Value: Boolean. Not String)
+
+  /** Optional */
+  googleAnalytics: '',     // Google Analytics TrackingID. ex.'UA-123456789-0'
+  googleSearchConsole: '', // content value in HTML tag of google search console ownership verification. ex.'w-K42k14_I4ApiQKuVPbCRVV-GxlrqWxYoqO94KMbKo'
+  googleAdsenseSlot: '',   // Google Adsense Slot. ex.'5214956675'
+  googleAdsenseClient: '', // Google Adsense Client. ex.'ca-pub-5001380215831339'
+    // Please correct the adsense client number(ex.5001380215831339) in the './static/ads.txt' file.
+};
